cc2a2ff6d2530ab948752790fa79e8c2888b00f5
Gonzalo <gonziibarberis@gmail.com>
2023-02-09 13:05:34 -0300

    only jersey

--- track.js
+++ track.js
@@ -12,8 +12,6 @@ const texto = []
 
 async function trackeo(){
 
-  console.log(i)
-
   const browser = await puppeteer.launch({
     headless: true,
     ignoreHTTPSErrors: true,
@@ -61,89 +59,13 @@ async function trackeo(){
     let fechaText = await page.evaluate((el) => el.textContent, fecha[0]);
     texto[2] = fechaText
     await new Promise((r) => setTimeout(r, 800));
+
+
+    cambioTrack(page, "")
   
     
-  
-  
-    await new Promise((r) => setTimeout(r, 3000));
-    cambioTrack(page, 'SP624028870AR')
 
-    await new Promise((r) => setTimeout(r, 6000));
-
-    info = await page.$x('//*[@id="track_item_90_SP624028870AR_0"]/div[2]/div/text()');
-    await new Promise((r) => setTimeout(r, 1000));
-    infoText = await page.evaluate((el) => el.textContent, info[0]);
-    texto[3] = infoText.toUpperCase()
-  
-    await new Promise((r) => setTimeout(r, 800));
-  
-    lugar = await page.$x('//*[@id="track_item_90_SP624028870AR_0"]/div[2]/div/span');
-    await new Promise((r) => setTimeout(r, 1000));
-    lugarText = await page.evaluate((el) => el.textContent, lugar[0]);
-    texto[4] = lugarText
-  
-    await new Promise((r) => setTimeout(r, 800));
-  
-    fecha = await page.$x('//*[@id="track_item_90_SP624028870AR_0"]/div[2]/time/text()');
-    await new Promise((r) => setTimeout(r, 1000));
-    fechaText = await page.evaluate((el) => el.textContent, fecha[0]);
-    texto[5] = fechaText
-    await new Promise((r) => setTimeout(r, 800));
-
-    await new Promise((r) => setTimeout(r, 3000));
-    cambioTrack(page, 'SP623979539AR')
-
-    await new Promise((r) => setTimeout(r, 6000));
-
-    info = await page.$x('//*[@id="track_item_90_SP623979539AR_0"]/div[2]/div/text()');
-    await new Promise((r) => setTimeout(r, 1000));
-    infoText = await page.evaluate((el) => el.textContent, info[0]);
-    texto[6] = infoText.toUpperCase()
-  
-    await new Promise((r) => setTimeout(r, 800));
-  
-    lugar = await page.$x('//*[@id="track_item_90_SP623979539AR_0"]/div[2]/div/span');
-    await new Promise((r) => setTimeout(r, 1000));
-    lugarText = await page.evaluate((el) => el.textContent, lugar[0]);
-    texto[7] = lugarText
-  
-    await new Promise((r) => setTimeout(r, 800));
-  
-    fecha = await page.$x('//*[@id="track_item_90_SP623979539AR_0"]/div[2]/time/text()');
-    await new Promise((r) => setTimeout(r, 1000));
-    fechaText = await page.evaluate((el) => el.textContent, fecha[0]);
-    texto[8] = fechaText
-    await new Promise((r) => setTimeout(r, 800));
-
-    await new Promise((r) => setTimeout(r, 3000));
-    cambioTrack(page, 'UY274479332CZ')
-
-    await new Promise((r) => setTimeout(r, 6000));
-
-    info = await page.$x('//*[@id="track_item_148_UY274479332CZ_0"]/div[2]/div/text()');
-    await new Promise((r) => setTimeout(r, 1000));
-    infoText = await page.evaluate((el) => el.textContent, info[0]);
-    texto[9] = infoText.toUpperCase()
-  
-    await new Promise((r) => setTimeout(r, 800));
-  
-    lugar = await page.$x('//*[@id="track_item_148_UY274479332CZ_0"]/div[2]/div/span');
-    await new Promise((r) => setTimeout(r, 1000));
-    lugarText = await page.evaluate((el) => el.textContent, lugar[0]);
-    texto[10] = lugarText
-  
-    await new Promise((r) => setTimeout(r, 800));
-  
-    fecha = await page.$x('//*[@id="track_item_148_UY274479332CZ_0"]/div[2]/time/text()');
-    await new Promise((r) => setTimeout(r, 1000));
-    fechaText = await page.evaluate((el) => el.textContent, fecha[0]);
-    texto[11] = fechaText
-    await new Promise((r) => setTimeout(r, 800));
-  
-
-   
-
-    let msg = `📦 ❗ <b><u>Nuevo movimiento</u></b>\n\n<b>✅Camiseta ARG:</b>\n<i> 🏤${texto[0]}\n 📍${texto[1]}\n 📅${texto[2]}</i>\n<b>✅Switches:</b>\n<i> 🏤${texto[3]}\n 📍${texto[4]}\n 📅${texto[5]}</i>\n<b>✅Keycaps:</b>\n<i> 🏤${texto[6]}\n 📍${texto[7]}\n 📅${texto[8]}</i>\n<b>✅Funda:</b>\n<i> 🏤${texto[9]}\n 📍${texto[10]}\n 📅${texto[11]}</i>`
+    let msg = `📦 ❗ <b><u>Nuevo movimiento</u></b>\n\n<b>Camiseta ARG:</b>\n<i>🏤${texto[0]}\n📍${texto[1]}\n📅${texto[2]}</i>`
 
 
 
@@ -153,7 +75,6 @@ async function trackeo(){
       console.log("Hubo un cambio en los avisos");
       bot.telegram.sendMessage(id, msg, { parse_mode: "HTML" });
       cambio = msg;
-      
     }
 
 
