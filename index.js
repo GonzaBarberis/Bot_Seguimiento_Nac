46c41d69cbf35252eab8c7d80b4c03cdbbf4c355
Gonzalo <gonziibarberis@gmail.com>
2024-04-16 12:39:49 -0300

    agregado backend

--- index.js
+++ index.js
@@ -1,79 +1,119 @@
 const puppeteer = require("puppeteer");
 const { Telegraf, Context } = require("telegraf");
+const fs = require('fs');
+const express = require('express');
+require('dotenv').config();
 
-const bot = new Telegraf("5906119682:AAFitZoSk51MOv4iU2GXbLYSTvUaXccEcL4");
-const id = "5760438151";
+// const bot = new Telegraf("5906119682:AAFitZoSk51MOv4iU2GXbLYSTvUaXccEcL4");
+// const id = "5760438151";
+
+
+const app = express();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const id = process.env.CHAT_ID
 
 let i = 0;
 let cambio = "";
-
-const texto = []
-
 let msg = "";
+
+
+app.get('/', async (req, res) => {
+
+
+  res.send('<h1 style="color:blue; text-align:center; font-family:\'Verdana\'">Iniciando Bot...</h1>');
+
+
+  trackeo().catch(error => {
+    console.error('Error al iniciar el bot:', error);
+  });
+
+
+});
+
+
+app.get('/test', async (req, res) => {
+  
+  res.send('<h1 style="color:blue; text-align:center; font-family:\'Verdana\'">Iniciando Bot...</h1>');
+
+});
+
+
+
 
 
 async function trackeo(){
 
+  
+
+  function guardarCookies(cookies) {
+    fs.writeFileSync('cookies.json', JSON.stringify(cookies));
+  }
+
+  function cargarCookies() {
+    return JSON.parse(fs.readFileSync('cookies.json'));
+  }
+
+
   const browser = await puppeteer.launch({
-    headless: false,
     ignoreHTTPSErrors: true,
+    headless: false,
     args: [
-      `--window-size=1920,1080`,
+      `--window-size=1200,800`,
       "--no-sandbox",
       "--disable-setuid-sandbox",
     ],
     defaultViewport: {
-      width: 1920,
-      height: 1080,
+      width: 1200,
+      height: 800,
     },
   });
+
 
   try{
     const page = await browser.newPage();
     await page.goto("https://chinapost-track.com/track-trace");
-    await new Promise((r) => setTimeout(r, 3000));
-  
+    
+    //await new Promise((r) => setTimeout(r, 4000));
+
+    const cookies = await page.cookies();
+    guardarCookies(cookies);
+    const cookiesGuardadas = cargarCookies();
+    await page.setCookie(...cookiesGuardadas);
+
     let input = await page.$x('//*[@id="track-form"]/input'); 
+
+    console.log("Iniciando clicks...")
+
+
     await input[0].click();
     await page.keyboard.type("RP108478134MU");
-    await new Promise((r) => setTimeout(r, 1000));
-    
-    await page.keyboard.press("Enter");
+    await new Promise((r) => setTimeout(r, 1800)); 
+    await page.keyboard.press("Enter");   
 
-    await new Promise((r) => setTimeout(r, 1000));
-
-
-    console.log('Por hacer click en enter 2da vez...');
+    await new Promise((r) => setTimeout(r, 3000));
 
     let input2 = await page.$x('//*[@id="track-form"]/input'); 
     await input2[0].click();
     await page.keyboard.type("RP108478134MU");
-    
+    await new Promise((r) => setTimeout(r, 1800)); 
+    await page.keyboard.press("Enter");   
 
-    await page.keyboard.press("Enter");
-
-  
-    await new Promise((r) => setTimeout(r, 10000));
-
+    await new Promise((r) => setTimeout(r, 15000));
 
     try {
     
-      // Obtener los elementos
       const infoElements = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/text()');
       const lugarElements = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/span');
       const fechaElements = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/time/text()');
     
-      // Verificar si se encontraron los elementos
       if (infoElements.length > 0 && lugarElements.length > 0 && fechaElements.length > 0) {
-        // Esperar un momento para asegurarse de que el contenido se haya cargado completamente
-        await page.waitForTimeout(1000);
+
+        await new Promise((r) => setTimeout(r, 1000));
     
-        // Obtener el texto de cada elemento
         const infoText = await page.evaluate((el) => el.textContent, infoElements[0]);
         const lugarText = await page.evaluate((el) => el.textContent, lugarElements[0]);
         const fechaText = await page.evaluate((el) => el.textContent, fechaElements[0]);
     
-        // Convertir el texto a mayúsculas si es necesario
         const textoInfo = infoText.toUpperCase();
         const textoLugar = lugarText.toUpperCase();
         const textoFecha = fechaText.toUpperCase();
@@ -91,17 +131,16 @@ async function trackeo(){
     } else {
       console.log("Hubo un cambio en los avisos");
       bot.telegram.sendMessage(id, msg, { parse_mode: "HTML" });
+      //console.log(msg)
       cambio = msg;
     }
 
-
-  
     await browser.close();
   }
   catch (error){
     await new Promise((r) => setTimeout(r, 2000));
     console.error(
-      "Hubo un error, intentando de nuevo dentro de 1hs 30min.", error.message
+      "Hubo un error, intentando de nuevo dentro de 1hs 30min.", error
     );
     await browser.close();
   }
@@ -110,100 +149,28 @@ async function trackeo(){
 }
 
 
-async function cambioTrack(page, number){
-  let input = await page.$x('//*[@id="track-form"]/input');
-  await input[0].click();
-  for (let j = 0; j < 20;j++){
-    await page.keyboard.press("Backspace");
-  }
-  await page.keyboard.type(number);
-  await new Promise((r) => setTimeout(r, 700));
-  await page.keyboard.press("Enter");
-  await new Promise((r) => setTimeout(r, 7000));
-}
-
 console.log("Iniciando...")
 
-trackeo()
+//trackeo()
 
-const interval = setInterval(() => {
-  //track();
-  trackeo()
-}, 1000 * 60 * 120);
+// const interval = setInterval(() => {
+//   trackeo()
+// }, 1000 * 60 * 120);
 
-const prueba = () =>{
+
+const port = process.env.PORT || 3600;
+  app.listen(port, () => {
+    console.log(`Servidor iniciado en el puerto ${port}`);
+});
+
+
+
+const pruebaMsj = () =>{
   bot.telegram.sendMessage(id, 'Test', { parse_mode: "HTML" });
 }
 
-//prueba()
+//pruebaMsj()
+
 
 
     
-    // let info = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/text()');    
-    // await new Promise((r) => setTimeout(r, 1000));
-    // let infoText = await page.evaluate((el) => el.textContent, info[0]);
-    // texto[0] = infoText.toUpperCase()
-  
-    // await new Promise((r) => setTimeout(r, 800));
-  
-    // let lugar = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/span');
-    // await new Promise((r) => setTimeout(r, 1000));
-    // let lugarText = await page.evaluate((el) => el.textContent, lugar[0]);
-    // texto[1] = lugarText
-  
-    // await new Promise((r) => setTimeout(r, 800));
-  
-    // let fecha = await page.$x('//*[@id="track_item_90_RP108478134MU_0"]/div[2]/time/text()');
-    // await new Promise((r) => setTimeout(r, 1000));
-    // let fechaText = await page.evaluate((el) => el.textContent, fecha[0]);
-    // texto[2] = fechaText
-    // await new Promise((r) => setTimeout(r, 800));
-
-
-    
-
-
-
-
-    // //cambioTrack(page, "CU773522090AR")
-    // console.log("Cambiando de tracking...")
-    // input = await page.$x('//*[@id="track-form"]/input');
-    // await input[0].click();
-    // for (let j = 0; j < 20;j++){
-    //   await page.keyboard.press("Backspace");
-    // }
-    // await page.keyboard.type("CU773522090AR");
-    // await new Promise((r) => setTimeout(r, 700));
-    // await page.keyboard.press("Enter");
-    // await new Promise((r) => setTimeout(r, 7000));
-
-    // info = await page.$x('//*[@id="track_item_90_CU773522090AR_0"]/div[2]/div/text()');
-    // await new Promise((r) => setTimeout(r, 1000));
-    // infoText = await page.evaluate((el) => el.textContent, info[0]);
-    // texto[0] = infoText.toUpperCase()
-  
-    // await new Promise((r) => setTimeout(r, 800));
-  
-    // lugar = await page.$x('//*[@id="track_item_90_CU773522090AR_0"]/div[2]/div/span');
-    // await new Promise((r) => setTimeout(r, 1000));
-    // lugarText = await page.evaluate((el) => el.textContent, lugar[0]);
-    // texto[1] = lugarText
-  
-    // await new Promise((r) => setTimeout(r, 800));
-  
-    // fecha = await page.$x('//*[@id="track_item_90_CU773522090AR_0"]/div[2]/time/text()');
-    // await new Promise((r) => setTimeout(r, 1000));
-    // fechaText = await page.evaluate((el) => el.textContent, fecha[0]);
-    // texto[2] = fechaText
-    // await new Promise((r) => setTimeout(r, 800));
-
-
-
-    // msg = msg + `\n<b>Zapas Vans:</b>\n<i>🏤${texto[0]}\n📍${texto[1]}\n📅${texto[2]}</i>`
-
-
-    
-    // Esperar a que aparezcan los elementos en la página
-    //await page.waitForXPath('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/text()');
-    // await page.waitForXPath('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/span');
-    // await page.waitForXPath('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/time/text()');
