89df9bbbd1a49bb9e70bcaa599f0075060656063
Gonzalo <gonziibarberis@gmail.com>
2024-04-17 18:02:26 -0300

    solucion de headless, no pup-core ni lambda

--- index.js
+++ index.js
@@ -1,55 +1,34 @@
-const puppeteer = require("puppeteer");
+//const puppeteer = require("puppeteer-core");
+//const chrome = require("chrome-aws-lambda")
 const { Telegraf, Context } = require("telegraf");
-const fs = require('fs');
-const express = require('express');
-require('dotenv').config();
+const fs = require("fs");
+// const express = require("express");
+require("dotenv").config();
 
 // const bot = new Telegraf("5906119682:AAFitZoSk51MOv4iU2GXbLYSTvUaXccEcL4");
 // const id = "5760438151";
 
+let chrome = {};
+let puppeteer;
 
-const app = express();
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const id = process.env.CHAT_ID
+const id = process.env.CHAT_ID;
 
 let i = 0;
 let cambio = "";
 let msg = "";
 
-
-trackeo()
-
-app.get('/', async (req, res) => {
-
-
-  res.send('<h1 style="color:blue; text-align:center; font-family:\'Verdana\'">Iniciando Bot...</h1>');
-
-
-  trackeo().catch(error => {
-    console.error('Error al iniciar el bot:', error);
-  });
-
-
-});
-
-
-app.get('/test', async (req, res) => {
-  
-  res.send('<h1 style="color:blue; text-align:center; font-family:\'Verdana\'">Iniciando Bot...</h1>');
-
-});
-
-
-
-
-
-async function trackeo(){
-
-    
-
+async function trackeo() {
   const browser = await puppeteer.launch({
     ignoreHTTPSErrors: true,
-    headless: false,
+    headless: "new",
     args: [
       `--window-size=1200,800`,
       "--no-sandbox",
@@ -61,102 +40,107 @@ async function trackeo(){
     },
   });
 
-
-  try{
+  try {
     const page = await browser.newPage();
     await page.goto("https://chinapost-track.com/track-trace");
 
+    await new Promise((r) => setTimeout(r, 2000));
 
-    let input = await page.$x('//*[@id="track-form"]/input'); 
+    let input = await page.$x('//*[@id="track-form"]/input');
 
-    console.log("Iniciando clicks...")
-
+    console.log("Iniciando clicks...");
 
     await input[0].click();
-    await page.keyboard.type("RP108478134MU");
-    await new Promise((r) => setTimeout(r, 1800)); 
-    await page.keyboard.press("Enter");   
+    await page.keyboard.type("RG023708764CN");
+    await new Promise((r) => setTimeout(r, 1800));
+    await page.keyboard.press("Enter");
 
-    await new Promise((r) => setTimeout(r, 3000));
+    // await new Promise((r) => setTimeout(r, 3000));
 
-    let input2 = await page.$x('//*[@id="track-form"]/input'); 
-    await input2[0].click();
-    await page.keyboard.type("RP108478134MU");
-    await new Promise((r) => setTimeout(r, 1800)); 
-    await page.keyboard.press("Enter");   
+    // let input2 = await page.$x('//*[@id="track-form"]/input');
+    // await input2[0].click();
+    // await page.keyboard.type("RG023708764CN");
+    // await new Promise((r) => setTimeout(r, 1800));
+    // await page.keyboard.press("Enter");
 
     await new Promise((r) => setTimeout(r, 15000));
 
     try {
-    
-      const infoElements = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/text()');
-      const lugarElements = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/div/span');
-      const fechaElements = await page.$x('//*[@id="track_item_78_RP108478134MU_0"]/div[2]/time/text()');
-    
-      if (infoElements.length > 0 && lugarElements.length > 0 && fechaElements.length > 0) {
+      const infoElements = await page.$x(
+        '//*[@id="track_item_6_RG023708764CN_0"]/div[2]/div/text() '
+      );
+      const lugarElements = await page.$x(
+        '//*[@id="track_item_6_RG023708764CN_0"]/div[2]/div/span'
+      );
+      const fechaElements = await page.$x(
+        '//*[@id="track_item_6_RG023708764CN_0"]/div[2]/time/text()'
+      );
 
+      if (
+        infoElements.length > 0 &&
+        lugarElements.length > 0 &&
+        fechaElements.length > 0
+      ) {
         await new Promise((r) => setTimeout(r, 1000));
-    
-        const infoText = await page.evaluate((el) => el.textContent, infoElements[0]);
-        const lugarText = await page.evaluate((el) => el.textContent, lugarElements[0]);
-        const fechaText = await page.evaluate((el) => el.textContent, fechaElements[0]);
-    
+
+        const infoText = await page.evaluate(
+          (el) => el.textContent,
+          infoElements[0]
+        );
+        const lugarText = await page.evaluate(
+          (el) => el.textContent,
+          lugarElements[0]
+        );
+        const fechaText = await page.evaluate(
+          (el) => el.textContent,
+          fechaElements[0]
+        );
+
         const textoInfo = infoText.toUpperCase();
         const textoLugar = lugarText.toUpperCase();
         const textoFecha = fechaText.toUpperCase();
 
-        msg = `📦 ❗ <b><u>Nuevo movimiento</u></b>\n\n<b>Camiseta Boca 🔵🟡:</b>\n<i>🏤 ${textoInfo}\n📍 ${textoLugar}\n📅 ${textoFecha}</i>`
+        msg = `📦 ❗ <b><u>Nuevo movimiento</u></b>\n\n<b>Camiseta Boca 🔵🟡:</b>\n<i>🏤 ${textoInfo}\n📍 ${textoLugar}\n📅 ${textoFecha}</i>`;
       } else {
-        console.error('No se encontraron todos los elementos que coinciden con las expresiones XPath.');
+        console.error(
+          "No se encontraron todos los elementos que coinciden con las expresiones XPath."
+        );
       }
     } catch (error) {
-      console.error('Hubo un error:', error);
+      console.error("Hubo un error:", error);
     }
-    
+
     if (msg === cambio) {
       console.log("No hay cambios");
     } else {
       console.log("Hubo un cambio en los avisos");
-      //bot.telegram.sendMessage(id, msg, { parse_mode: "HTML" });
+      bot.telegram.sendMessage(id, msg, { parse_mode: "HTML" });
       cambio = msg;
     }
 
     await browser.close();
-  }
-  catch (error){
+  } catch (error) {
     await new Promise((r) => setTimeout(r, 2000));
     console.error(
-      "Hubo un error, intentando de nuevo dentro de 1hs 30min.", error
+      "Hubo un error, intentando de nuevo dentro de 1hs 30min.",
+      error
     );
     await browser.close();
   }
-  
- 
 }
 
+console.log("Iniciando...");
 
-console.log("Iniciando...")
-
-//trackeo()
+trackeo();
 
 // const interval = setInterval(() => {
 //   trackeo()
 // }, 1000 * 60 * 120);
 
+const pruebaMsj = () => {
+  msg = `📦 ❗ <b><u>Nuevo movimiento</u></b>\n\n<b>Camiseta Boca 🔵🟡:</b>\n<i>🏤 CORREO\n📍 SEOUL\n📅 17/04/2024</i>`;
 
-const port = process.env.PORT || 3600;
-  app.listen(port, () => {
-    console.log(`Servidor iniciado en el puerto ${port}`);
-});
+  bot.telegram.sendMessage(id, msg, { parse_mode: "HTML" });
+};
 
-
-
-const pruebaMsj = () =>{
-  bot.telegram.sendMessage(id, 'Test', { parse_mode: "HTML" });
-}
-
-//pruebaMsj()
-
-
-
-    
+//pruebaMsj();
